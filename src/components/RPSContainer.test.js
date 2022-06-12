a0db5145b1d58bfe5a6831c68f7c601b8fb7c04c
BudhadityaP <budhadityapandit@gmail.com>
2022-06-12 10:59:22 +0100

    Unit testing started.

--- src/components/RPSContainer.test.js
+++ src/components/RPSContainer.test.js
@@ -1,17 +1,42 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import RPSContainer from './RPSContainer';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { render, screen, configure } from '@testing-library/react';
+import RPSContainer from './RPSContainer';
+import { BrowserRouter } from "react-router-dom";
+import userEvent from '@testing-library/user-event'
 
-// let container;
-// describe('----- RPSContainer tests -----', () => {
-//     it('renders correctly', () => {
-//         //const root = createRoot(document.getElementById('root'));
-//         // act(() => {
-//         //     ReactDOM.createRoot(container).render(<BrowserRouter><RPSContainer /></BrowserRouter>);
-//         //   });
-//         render(<BrowserRouter><RPSContainer /></BrowserRouter>);
-//           const h1 = container.querySelector('h1');
-//           expect(h1.textContent).toBe('Welcome to the game - Rock Paper Scissors!');
-//     })
-// })
+const testRender = (component) => {
+    return (render(<BrowserRouter>{component}</BrowserRouter>))
+}
+let container;
+describe('----- RPSContainer tests -----', () => {
+    beforeEach(() => {
+        configure({
+            throwSuggestions: true,
+        })
+        container = testRender(<RPSContainer />);
+
+    })
+
+    it('renders correctly - header exists', () => {
+        expect(container).toMatchSnapshot()
+        const h1 = screen.getByRole('heading', { name: /welcome to the game - rock paper scissors!/i })
+        expect(h1).toBeDefined();
+
+    })
+
+    it('renders correctly - buttons exist', () => {
+        const button1 = screen.getByRole('button', { name: /human vs computer/i })
+        const button2 = screen.getByRole('button', { name: /comp vs comp/i })
+        expect(button1).toBeDefined();
+        expect(button2).toBeDefined();
+    })
+
+    xit('renders correctly - Users clicks Human vs Computer button', async () => {
+        const user = userEvent.setup()
+        const button1 = screen.getByRole('button', { name: /human vs computer/i })
+        expect(button1).toBeDefined();
+       await user.click(button1)
+        // const headingPWC = screen.getByRole('heading', { name: /play with computer/i })
+        // expect(headingPWC).toBeInTheDocument();
+    })
+})
